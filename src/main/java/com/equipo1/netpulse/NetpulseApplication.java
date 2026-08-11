23cdc7f9e550834074010bf00afb67f745f5e29e
Angry548 <angelmate827@gmail.com>
2026-08-10 22:05:35 -0600

    chore: estructura inicial del proyecto

---
+++ src/main/java/com/equipo1/netpulse/NetpulseApplication.java
@@ -0,0 +1,12 @@
+package com.equipo1.netpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetpulseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/equipo1/netpulse/NetpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo1.netpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
