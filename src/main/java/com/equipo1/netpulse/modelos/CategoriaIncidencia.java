563a16e86e616c80f6ad567047dc49cf2241f8e1
Edwin Fabricio Arévalo Ávila <edwinfabricioarevaloavila675@gmail.com>
2026-08-18 08:30:52 -0600

    Fix: CarlEdwin FabricioCorrección de error tipográfico en el módulo de CategoriaIncidencia

--- src/main/java/com/equipo1/netpulse/modelos/CategoriaIncidencia.java
+++ src/main/java/com/equipo1/netpulse/modelos/CategoriaIncidencia.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+
 @Entity
 @Table(name = "categorias_incidencia")
 public class CategoriaIncidencia {
