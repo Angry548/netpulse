9691c0cd11909300d48da14c6f6f2eece69f6261
Edwin Fabricio Arévalo Ávila <edwinfabricioarevaloavila675@gmail.com>
2026-08-18 11:02:44 -0600

    [SG-57] Edwin Fabricio : Crear IHistorialEstadoRepository - Esto permitira consultar y gestionar el historial de cambios de estado de los equipos.

---
+++ src/main/java/com/equipo1/netpulse/repositorios/IHistorialEstadoRepository.java
@@ -0,0 +1,8 @@
+package com.equipo1.netpulse.repositorios;
+
+import com.equipo1.netpulse.modelos.HistorialEstado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IHistorialEstadoRepository extends JpaRepository<HistorialEstado, Integer> {
+}
+
