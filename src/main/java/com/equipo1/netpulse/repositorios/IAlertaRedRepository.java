99dfbf53afb149f403827d9acc7d2450afcaaa6c
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-08-17 23:05:58 -0600

    [SG-58] Karla Ceren : Crear IAlertaRedRepository - Esto permitira almacenar y consultar las alertas generadas por los equipos.

---
+++ src/main/java/com/equipo1/netpulse/repositorios/IAlertaRedRepository.java
@@ -0,0 +1,7 @@
+package com.equipo1.netpulse.repositorios;
+
+import com.equipo1.netpulse.modelos.AlertaRed;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IAlertaRedRepository extends JpaRepository<AlertaRed, Integer> {
+}
