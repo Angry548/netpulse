5d76725fc22a1a8f00654d44c27b2b23dca877a4
Angry548 <angelmate827@gmail.com>
2026-08-18 11:35:46 -0600

    [SG-53] Jose Angel - Crear IRolRepository - Implementa operaciones CRUD sobre los roles almacenados en la base de datos.

---
+++ src/main/java/com/equipo1/netpulse/repositorios/IRolRepository.java
@@ -0,0 +1,7 @@
+package com.equipo1.netpulse.repositorios;
+
+import com.equipo1.netpulse.modelos.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRolRepository extends JpaRepository<Rol, Integer> {
+}
