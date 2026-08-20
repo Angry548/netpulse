09d147a6131d9a55ea8d7983003f1ec5f6c62a85
Angry548 <angelmate827@gmail.com>
2026-08-20 10:39:45 -0600

    [SG-70] Jose Angel : Crear servicio de Equipo - Gestionar equipos y sus operaciones relacionadas.

--- src/main/java/com/equipo1/netpulse/servicios/interfaces/IEquipoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/interfaces/IEquipoService.java
@@ -1,4 +1,22 @@
 package com.equipo1.netpulse.servicios.interfaces;
 
-public class IEquipoService {
+import com.equipo1.netpulse.modelos.Equipo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface IEquipoService {
+
+    Page<Equipo> obtenerTodosPaginados(Pageable pageable);
+
+    List<Equipo> obtenerTodos();
+
+    Equipo obtenerPorId(Integer id);
+
+    Equipo obtenerPorNumeroSerie(String numeroSerie);
+
+    Equipo crearOEditar(Equipo equipo);
+
+    void eliminarPorId(Integer id);
 }
