08d82cb31b05779a4678a6ba938e20293343bf9d
Angry548 <angelmate827@gmail.com>
2026-08-20 10:36:10 -0600

    [SG-68] Jose Angel : Crear servicio de TipoEquipo - Gestionar los tipos de equipo mediante su repositorio.

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/TipoEquipoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/TipoEquipoService.java
@@ -1,4 +1,48 @@
 package com.equipo1.netpulse.servicios.implementaciones;
 
-public class TipoEquipoService {
+import com.equipo1.netpulse.modelos.TipoEquipo;
+import com.equipo1.netpulse.repositorios.ITipoEquipoRepository;
+import com.equipo1.netpulse.servicios.interfaces.ITipoEquipoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TipoEquipoService implements ITipoEquipoService {
+
+    @Autowired
+    private ITipoEquipoRepository tipoEquipoRepository;
+
+    @Override
+    public Page<TipoEquipo> obtenerTodosPaginados(Pageable pageable) {
+        return tipoEquipoRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<TipoEquipo> obtenerTodos() {
+        return tipoEquipoRepository.findAll();
+    }
+
+    @Override
+    public TipoEquipo obtenerPorId(Integer id) {
+        return tipoEquipoRepository.findById(id).get();
+    }
+
+    @Override
+    public TipoEquipo crearOEditar(TipoEquipo tipoEquipo) {
+        return tipoEquipoRepository.save(tipoEquipo);
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+        tipoEquipoRepository.deleteById(id);
+    }
+
+    @Override
+    public TipoEquipo obtenerPorNombre(String nombre) {
+        return tipoEquipoRepository.findByNombre(nombre).orElse(null);
+    }
 }
--- src/main/java/com/equipo1/netpulse/servicios/interfaces/ITipoEquipoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/interfaces/ITipoEquipoService.java
@@ -1,4 +1,22 @@
 package com.equipo1.netpulse.servicios.interfaces;
 
-public class ITipoEquipoService {
+import com.equipo1.netpulse.modelos.TipoEquipo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ITipoEquipoService {
+
+    Page<TipoEquipo> obtenerTodosPaginados(Pageable pageable);
+
+    List<TipoEquipo> obtenerTodos();
+
+    TipoEquipo obtenerPorId(Integer id);
+
+    TipoEquipo crearOEditar(TipoEquipo tipoEquipo);
+
+    void eliminarPorId(Integer id);
+
+    TipoEquipo obtenerPorNombre(String nombre);
 }
