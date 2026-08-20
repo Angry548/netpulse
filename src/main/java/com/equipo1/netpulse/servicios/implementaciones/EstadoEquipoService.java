f50727923835d2ffbe0ee144012bf3b71904e248
Angry548 <angelmate827@gmail.com>
2026-08-20 10:38:04 -0600

    [SG-69] Jose Angel : Crear servicio de EstadoEquipo - Gestionar los estados de los equipos.

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/EstadoEquipoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/EstadoEquipoService.java
@@ -1,4 +1,48 @@
 package com.equipo1.netpulse.servicios.implementaciones;
 
-public class EstadoEquipoService {
+import com.equipo1.netpulse.modelos.EstadoEquipo;
+import com.equipo1.netpulse.repositorios.IEstadoEquipoRepository;
+import com.equipo1.netpulse.servicios.interfaces.IEstadoEquipoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EstadoEquipoService implements IEstadoEquipoService {
+
+    @Autowired
+    private IEstadoEquipoRepository estadoEquipoRepository;
+
+    @Override
+    public Page<EstadoEquipo> obtenerTodosPaginados(Pageable pageable) {
+        return estadoEquipoRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<EstadoEquipo> obtenerTodos() {
+        return estadoEquipoRepository.findAll();
+    }
+
+    @Override
+    public EstadoEquipo obtenerPorId(Integer id) {
+        return estadoEquipoRepository.findById(id).get();
+    }
+
+    @Override
+    public EstadoEquipo crearOEditar(EstadoEquipo estadoEquipo) {
+        return estadoEquipoRepository.save(estadoEquipo);
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+        estadoEquipoRepository.deleteById(id);
+    }
+
+    @Override
+    public EstadoEquipo obtenerPorNombre(String nombre) {
+        return estadoEquipoRepository.findByNombre(nombre).orElse(null);
+    }
 }
--- src/main/java/com/equipo1/netpulse/servicios/interfaces/IEstadoEquipoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/interfaces/IEstadoEquipoService.java
@@ -1,4 +1,22 @@
 package com.equipo1.netpulse.servicios.interfaces;
 
-public class IEstadoEquipoService {
+import com.equipo1.netpulse.modelos.EstadoEquipo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface IEstadoEquipoService {
+
+    Page<EstadoEquipo> obtenerTodosPaginados(Pageable pageable);
+
+    List<EstadoEquipo> obtenerTodos();
+
+    EstadoEquipo obtenerPorId(Integer id);
+
+    EstadoEquipo crearOEditar(EstadoEquipo estadoEquipo);
+
+    void eliminarPorId(Integer id);
+
+    EstadoEquipo obtenerPorNombre(String nombre);
 }
