886c96d3f3cfd53995dda66bbc330b8ae63f347f
Angry548 <angelmate827@gmail.com>
2026-08-20 10:23:19 -0600

    [SG-65] Jose Angel : Crear servicio de Usuario - Gestionar las operaciones de los usuarios mediante IUsuarioRepository.

--- src/main/java/com/equipo1/netpulse/servicios/interfaces/IUsuarioService.java
+++ src/main/java/com/equipo1/netpulse/servicios/interfaces/IUsuarioService.java
@@ -1,4 +1,23 @@
 package com.equipo1.netpulse.servicios.interfaces;
 
-public class IUsuarioService {
+import com.equipo1.netpulse.modelos.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUsuarioService {
+
+    Page<Usuario> obtenerTodosPaginados(Pageable pageable);
+
+    List<Usuario> obtenerTodos();
+
+    Usuario obtenerPorId(Integer id);
+
+    Optional<Usuario> obtenerPorCorreo(String correo);
+
+    Usuario crearOEditar(Usuario usuario);
+
+    void eliminarPorId(Integer id);
 }
