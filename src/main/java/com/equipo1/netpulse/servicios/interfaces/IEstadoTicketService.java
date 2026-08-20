e156f2545fa666dcd04709d9b1e13406cbb88e4d
Edwin Fabricio Arévalo Ávila <edwinfabricioarevaloavila675@gmail.com>
2026-08-20 09:42:05 -0600

    [SG-73] Edwin Fabricio : Crear servicio de EstadoTicket - Gestionar los estados de los tickets.

--- src/main/java/com/equipo1/netpulse/servicios/interfaces/IEstadoTicketService.java
+++ src/main/java/com/equipo1/netpulse/servicios/interfaces/IEstadoTicketService.java
@@ -1,4 +1,22 @@
 package com.equipo1.netpulse.servicios.interfaces;
 
-public class IEstadoTicketService {
+import com.equipo1.netpulse.modelos.EstadoTicket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface IEstadoTicketService {
+
+    List<EstadoTicket> obtenerTodos();
+
+    Page<EstadoTicket> buscarTodosPaginados(Pageable pageable);
+
+    EstadoTicket buscarPorId(Integer id);
+
+    EstadoTicket buscarPorNombre(String nombre);
+
+    EstadoTicket crearOEditar(EstadoTicket estadoTicket);
+
+    void eliminarPorId(Integer id);
 }
