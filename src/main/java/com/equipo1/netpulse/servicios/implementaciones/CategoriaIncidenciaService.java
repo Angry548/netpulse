0303bb8aa2d0be4efcc5b0274fa7d30fb5a73034
Edwin Fabricio Arévalo Ávila <edwinfabricioarevaloavila675@gmail.com>
2026-08-20 07:51:15 -0600

    [SG-71] Edwin Fabricio : Crear servicio de CategoriaIncidencia - Gestionar las categorías de incidencias.

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/CategoriaIncidenciaService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/CategoriaIncidenciaService.java
@@ -1,4 +1,48 @@
 package com.equipo1.netpulse.servicios.implementaciones;
 
-public class CategoriaIncidenciaService {
+import com.equipo1.netpulse.modelos.CategoriaIncidencia;
+import com.equipo1.netpulse.repositorios.ICategoriaIncidenciaRepository;
+import com.equipo1.netpulse.servicios.interfaces.ICategoriaIncidenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriaIncidenciaService implements ICategoriaIncidenciaService {
+
+    @Autowired
+    private ICategoriaIncidenciaRepository categoriaIncidenciaRepository;
+
+    @Override
+    public Page<CategoriaIncidencia> obtenerTodosPaginados(Pageable pageable) {
+        return categoriaIncidenciaRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<CategoriaIncidencia> obtenerTodos() {
+        return categoriaIncidenciaRepository.findAll();
+    }
+
+    @Override
+    public CategoriaIncidencia obtenerPorId(Integer id) {
+        return categoriaIncidenciaRepository.findById(id).get();
+    }
+
+    @Override
+    public CategoriaIncidencia obtenerPorNombre(String nombre) {
+        return categoriaIncidenciaRepository.findByNombre(nombre).orElse(null);
+    }
+
+    @Override
+    public CategoriaIncidencia crearOEditar(CategoriaIncidencia categoriaIncidencia) {
+        return categoriaIncidenciaRepository.save(categoriaIncidencia);
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+        categoriaIncidenciaRepository.deleteById(id);
+    }
 }
