7fb8ffd9e591c35390415cce58cf0487a9e04547
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-08-19 22:15:42 -0600

    [SG-76] Karla Ceren : Crear servicio de AlertaRed - Gestionar las alertas generadas por los equipos.

--- src/main/java/com/equipo1/netpulse/servicios/interfaces/IAlertaRedService.java
+++ src/main/java/com/equipo1/netpulse/servicios/interfaces/IAlertaRedService.java
@@ -1,4 +1,20 @@
 package com.equipo1.netpulse.servicios.interfaces;
 
-public class IAlertaRedService {
+import com.equipo1.netpulse.modelos.AlertaRed;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface IAlertaRedService {
+
+    List<AlertaRed> obtenerTodos();
+
+    Page<AlertaRed> buscarTodosPaginados(Pageable pageable);
+
+    AlertaRed buscarPorId(Integer id);
+
+    AlertaRed crearOEditar(AlertaRed alertaRed);
+
+    void eliminarPorId(Integer id);
 }
