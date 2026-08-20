87d79a413ce6cac3e090ea261d0b9ad995e3ada2
Angry548 <angelmate827@gmail.com>
2026-08-20 10:25:31 -0600

    [SG-66] Jose Angel : Crear servicio de Rol - Gestionar las operaciones de los roles mediante IRolRepository.

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/RolService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/RolService.java
@@ -1,4 +1,48 @@
 package com.equipo1.netpulse.servicios.implementaciones;
 
-public class RolService {
+import com.equipo1.netpulse.modelos.Rol;
+import com.equipo1.netpulse.repositorios.IRolRepository;
+import com.equipo1.netpulse.servicios.interfaces.IRolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RolService implements IRolService {
+
+    @Autowired
+    private IRolRepository rolRepository;
+
+    @Override
+    public Page<Rol> obtenerTodosPaginados(Pageable pageable) {
+        return rolRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Rol> obtenerTodos() {
+        return rolRepository.findAll();
+    }
+
+    @Override
+    public Rol obtenerPorId(Integer id) {
+        return rolRepository.findById(id).get();
+    }
+
+    @Override
+    public Rol crearOEditar(Rol rol) {
+        return rolRepository.save(rol);
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+        rolRepository.deleteById(id);
+    }
+
+    @Override
+    public Rol obtenerPorNombre(String nombre) {
+        return rolRepository.findByNombre(nombre).orElse(null);
+    }
 }
