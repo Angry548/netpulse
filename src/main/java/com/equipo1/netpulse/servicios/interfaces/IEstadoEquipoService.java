a7bfcd553a4771a95a8cc3a022e2af1623addc79
Angry548 <angelmate827@gmail.com>
2026-08-25 11:48:36 -0600

    feat(service) Jose Angel Mate Carias : Modificacion del servicio de EstadoEquipo

--- src/main/java/com/equipo1/netpulse/servicios/interfaces/IEstadoEquipoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/interfaces/IEstadoEquipoService.java
@@ -1,22 +1,20 @@
 package com.equipo1.netpulse.servicios.interfaces;
 
 import com.equipo1.netpulse.modelos.EstadoEquipo;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IEstadoEquipoService {
 
-    Page<EstadoEquipo> obtenerTodosPaginados(Pageable pageable);
+    EstadoEquipo crear(EstadoEquipo estadoEquipo);
+
+    EstadoEquipo buscarPorId(Integer id);
+
+    EstadoEquipo buscarPorNombre(String nombre);
 
     List<EstadoEquipo> obtenerTodos();
 
-    EstadoEquipo obtenerPorId(Integer id);
-
-    EstadoEquipo crearOEditar(EstadoEquipo estadoEquipo);
+    EstadoEquipo actualizar(EstadoEquipo estadoEquipo);
 
     void eliminarPorId(Integer id);
-
-    EstadoEquipo obtenerPorNombre(String nombre);
 }
