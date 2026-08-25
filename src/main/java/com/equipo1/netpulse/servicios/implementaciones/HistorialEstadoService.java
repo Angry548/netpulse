6c79f0a68c9747f72759991c5eb0ecf3e3a65d2f
edfabri1212 <edwinfabricioarevaloavila675@gmail.com>
2026-08-24 21:54:17 -0600

    [SG-77] Edwin Fabricio Arévalo Avila : Crear servicio de HistorialEstado - Gestionar el historial de cambios de estado de los equipos.

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/HistorialEstadoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/HistorialEstadoService.java
@@ -1,4 +1,43 @@
 package com.equipo1.netpulse.servicios.implementaciones;
 
-public class HistorialEstadoService {
+import com.equipo1.netpulse.modelos.HistorialEstado;
+import com.equipo1.netpulse.repositorios.IHistorialEstadoRepository;
+import com.equipo1.netpulse.servicios.interfaces.IHistorialEstadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HistorialEstadoService implements IHistorialEstadoService {
+
+    @Autowired
+    private IHistorialEstadoRepository historialEstadoRepository;
+
+    @Override
+    public List<HistorialEstado> obtenerTodos() {
+        return historialEstadoRepository.findAll();
+    }
+
+    @Override
+    public Page<HistorialEstado> buscarTodosPaginados(Pageable pageable) {
+        return historialEstadoRepository.findAll(pageable);
+    }
+
+    @Override
+    public HistorialEstado buscarPorId(Integer id) {
+        return historialEstadoRepository.findById(id).get();
+    }
+
+    @Override
+    public HistorialEstado crearOEditar(HistorialEstado historialEstado) {
+        return historialEstadoRepository.save(historialEstado);
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+        historialEstadoRepository.deleteById(id);
+    }
 }
