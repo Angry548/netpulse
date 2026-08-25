c32683dfcb0f50b41a9da36a1ef2c17b5c920c2f
Daniela Campos <danielaroblox1617@gmail.com>
2026-08-25 11:28:22 -0600

    feat(service) Daniela Tatiana Campos Mancia : Modificacion del servicio de Mantenimiento

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/MantenimientoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/MantenimientoService.java
@@ -4,8 +4,6 @@ import com.equipo1.netpulse.modelos.Mantenimiento;
 import com.equipo1.netpulse.repositorios.IMantenimientoRepository;
 import com.equipo1.netpulse.servicios.interfaces.IMantenimientoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,13 +15,8 @@ public class MantenimientoService implements IMantenimientoService {
     private IMantenimientoRepository mantenimientoRepository;
 
     @Override
-    public List<Mantenimiento> obtenerTodos() {
-        return mantenimientoRepository.findAll();
-    }
-
-    @Override
-    public Page<Mantenimiento> buscarTodosPaginados(Pageable pageable) {
-        return mantenimientoRepository.findAll(pageable);
+    public Mantenimiento registrar(Mantenimiento mantenimiento) {
+        return mantenimientoRepository.save(mantenimiento);
     }
 
     @Override
@@ -32,7 +25,17 @@ public class MantenimientoService implements IMantenimientoService {
     }
 
     @Override
-    public Mantenimiento crearOEditar(Mantenimiento mantenimiento) {
+    public List<Mantenimiento> obtenerTodos() {
+        return mantenimientoRepository.findAll();
+    }
+
+    @Override
+    public Mantenimiento actualizar(Mantenimiento mantenimiento) {
+        return mantenimientoRepository.save(mantenimiento);
+    }
+
+    @Override
+    public Mantenimiento finalizar(Mantenimiento mantenimiento) {
         return mantenimientoRepository.save(mantenimiento);
     }
 
--- src/main/java/com/equipo1/netpulse/servicios/interfaces/IMantenimientoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/interfaces/IMantenimientoService.java
@@ -1,20 +1,20 @@
 package com.equipo1.netpulse.servicios.interfaces;
 
 import com.equipo1.netpulse.modelos.Mantenimiento;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IMantenimientoService {
 
-    List<Mantenimiento> obtenerTodos();
-
-    Page<Mantenimiento> buscarTodosPaginados(Pageable pageable);
+    Mantenimiento registrar(Mantenimiento mantenimiento);
 
     Mantenimiento buscarPorId(Integer id);
 
-    Mantenimiento crearOEditar(Mantenimiento mantenimiento);
+    List<Mantenimiento> obtenerTodos();
+
+    Mantenimiento actualizar(Mantenimiento mantenimiento);
+
+    Mantenimiento finalizar(Mantenimiento mantenimiento);
 
     void eliminarPorId(Integer id);
 }
