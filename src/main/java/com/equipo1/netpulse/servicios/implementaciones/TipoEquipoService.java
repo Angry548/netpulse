40c80a9bad58dc96d1d0e353092fa6fdb10257e4
Angry548 <angelmate827@gmail.com>
2026-08-25 11:46:57 -0600

    feat(service) Jose Angel Mate Carias : Modificacion del servicio de TipoEquipo

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/TipoEquipoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/TipoEquipoService.java
@@ -4,8 +4,6 @@ import com.equipo1.netpulse.modelos.TipoEquipo;
 import com.equipo1.netpulse.repositorios.ITipoEquipoRepository;
 import com.equipo1.netpulse.servicios.interfaces.ITipoEquipoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,8 +15,18 @@ public class TipoEquipoService implements ITipoEquipoService {
     private ITipoEquipoRepository tipoEquipoRepository;
 
     @Override
-    public Page<TipoEquipo> obtenerTodosPaginados(Pageable pageable) {
-        return tipoEquipoRepository.findAll(pageable);
+    public TipoEquipo crear(TipoEquipo tipoEquipo) {
+        return tipoEquipoRepository.save(tipoEquipo);
+    }
+
+    @Override
+    public TipoEquipo buscarPorId(Integer id) {
+        return tipoEquipoRepository.findById(id).get();
+    }
+
+    @Override
+    public TipoEquipo buscarPorNombre(String nombre) {
+        return tipoEquipoRepository.findByNombre(nombre).orElse(null);
     }
 
     @Override
@@ -27,22 +35,12 @@ public class TipoEquipoService implements ITipoEquipoService {
     }
 
     @Override
-    public TipoEquipo obtenerPorId(Integer id) {
-        return tipoEquipoRepository.findById(id).get();
-    }
-
-    @Override
-    public TipoEquipo crearOEditar(TipoEquipo tipoEquipo) {
+    public TipoEquipo actualizar(TipoEquipo tipoEquipo) {
         return tipoEquipoRepository.save(tipoEquipo);
     }
 
     @Override
     public void eliminarPorId(Integer id) {
         tipoEquipoRepository.deleteById(id);
-    }
-
-    @Override
-    public TipoEquipo obtenerPorNombre(String nombre) {
-        return tipoEquipoRepository.findByNombre(nombre).orElse(null);
     }
 }
