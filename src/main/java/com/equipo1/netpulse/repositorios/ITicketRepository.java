ac350343bcad5e454bb63563282ffc38e97a8565
Edwin Fabricio Arévalo Ávila <edwinfabricioarevaloavila675@gmail.com>
2026-08-27 17:40:09 -0600

    feat(repository) Edwin Fabricio Arévalo Ávila : Modificacion del repositorio deI TicketRepository

--- src/main/java/com/equipo1/netpulse/repositorios/ITicketRepository.java
+++ src/main/java/com/equipo1/netpulse/repositorios/ITicketRepository.java
@@ -1,7 +1,31 @@
 package com.equipo1.netpulse.repositorios;
 
 import com.equipo1.netpulse.modelos.Ticket;
+import com.equipo1.netpulse.modelos.Usuario;
+import com.equipo1.netpulse.modelos.Equipo;
+import com.equipo1.netpulse.modelos.CategoriaIncidencia;
+import com.equipo1.netpulse.modelos.PrioridadTicket;
+import com.equipo1.netpulse.modelos.EstadoTicket;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ITicketRepository extends JpaRepository<Ticket, Integer> {
+
+    Page<Ticket> findAll(Pageable pageable);
+
+    List<Ticket> findByUsuarioReporta(Usuario usuario);
+
+    List<Ticket> findByUsuarioTecnico(Usuario usuario);
+
+    List<Ticket> findByEquipo(Equipo equipo);
+
+    List<Ticket> findByCategoria(CategoriaIncidencia categoria);
+
+    List<Ticket> findByPrioridad(PrioridadTicket prioridad);
+
+    List<Ticket> findByEstado(EstadoTicket estado);
 }
