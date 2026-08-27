50a37fadc0a7d4f52aa91cd83edb01716c8160e2
Edwin Fabricio Arévalo Ávila <edwinfabricioarevaloavila675@gmail.com>
2026-08-27 17:46:10 -0600

    feat(service) Edwin Fabricio Arévalo Ávila : Modificacion del servicio de TicketService

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/TicketService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/TicketService.java
@@ -1,9 +1,18 @@
 package com.equipo1.netpulse.servicios.implementaciones;
 
 import com.equipo1.netpulse.modelos.Ticket;
+import com.equipo1.netpulse.modelos.Usuario;
+import com.equipo1.netpulse.modelos.Equipo;
+import com.equipo1.netpulse.modelos.CategoriaIncidencia;
+import com.equipo1.netpulse.modelos.PrioridadTicket;
+import com.equipo1.netpulse.modelos.EstadoTicket;
+
 import com.equipo1.netpulse.repositorios.ITicketRepository;
 import com.equipo1.netpulse.servicios.interfaces.ITicketService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,6 +36,41 @@ public class TicketService implements ITicketService {
     @Override
     public List<Ticket> obtenerTodos() {
         return ticketRepository.findAll();
+    }
+
+    @Override
+    public Page<Ticket> buscarTodosPaginados(Pageable pageable) {
+        return ticketRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Ticket> obtenerPorUsuarioReporta(Usuario usuario) {
+        return ticketRepository.findByUsuarioReporta(usuario);
+    }
+
+    @Override
+    public List<Ticket> obtenerPorTecnico(Usuario usuario) {
+        return ticketRepository.findByUsuarioTecnico(usuario);
+    }
+
+    @Override
+    public List<Ticket> obtenerPorEquipo(Equipo equipo) {
+        return ticketRepository.findByEquipo(equipo);
+    }
+
+    @Override
+    public List<Ticket> obtenerPorCategoria(CategoriaIncidencia categoria) {
+        return ticketRepository.findByCategoria(categoria);
+    }
+
+    @Override
+    public List<Ticket> obtenerPorPrioridad(PrioridadTicket prioridad) {
+        return ticketRepository.findByPrioridad(prioridad);
+    }
+
+    @Override
+    public List<Ticket> obtenerPorEstado(EstadoTicket estado) {
+        return ticketRepository.findByEstado(estado);
     }
 
     @Override
