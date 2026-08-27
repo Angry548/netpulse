9f41dae8133a94de9e299d133ebfcd54c64a0291
Edwin Fabricio Arévalo Ávila <edwinfabricioarevaloavila675@gmail.com>
2026-08-27 17:48:18 -0600

    feat(repository) Edwin Fabricio Arévalo Ávila : Modificacion del repositorio deI ICategoriaIncidenciaRepository

--- src/main/java/com/equipo1/netpulse/repositorios/ICategoriaIncidenciaRepository.java
+++ src/main/java/com/equipo1/netpulse/repositorios/ICategoriaIncidenciaRepository.java
@@ -1,6 +1,8 @@
 package com.equipo1.netpulse.repositorios;
 
 import com.equipo1.netpulse.modelos.CategoriaIncidencia;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,6 @@ import java.util.Optional;
 public interface ICategoriaIncidenciaRepository extends JpaRepository<CategoriaIncidencia, Integer> {
 
     Optional<CategoriaIncidencia> findByNombre(String nombre);
+
+    Page<CategoriaIncidencia> findAll(Pageable pageable);
 }
