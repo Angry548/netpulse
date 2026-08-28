4477567fa1124e2d13a7171124739979bc1f21f4
Daniela Campos <danielaroblox1617@gmail.com>
2026-08-27 23:09:14 -0600

    feat(service) Daniela Tatiana Campos Mancia : Modificacion del servicio de MantenimientoService

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/MantenimientoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/MantenimientoService.java
@@ -1,9 +1,16 @@
 package com.equipo1.netpulse.servicios.implementaciones;
 
 import com.equipo1.netpulse.modelos.Mantenimiento;
+import com.equipo1.netpulse.modelos.Equipo;
+import com.equipo1.netpulse.modelos.Usuario;
+import com.equipo1.netpulse.modelos.Ticket;
+
 import com.equipo1.netpulse.repositorios.IMantenimientoRepository;
 import com.equipo1.netpulse.servicios.interfaces.IMantenimientoService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,12 +37,27 @@ public class MantenimientoService implements IMantenimientoService {
     }
 
     @Override
-    public Mantenimiento actualizar(Mantenimiento mantenimiento) {
-        return mantenimientoRepository.save(mantenimiento);
+    public Page<Mantenimiento> buscarTodosPaginados(Pageable pageable) {
+        return mantenimientoRepository.findAll(pageable);
     }
 
     @Override
-    public Mantenimiento finalizar(Mantenimiento mantenimiento) {
+    public List<Mantenimiento> obtenerPorEquipo(Equipo equipo) {
+        return mantenimientoRepository.findByEquipo(equipo);
+    }
+
+    @Override
+    public List<Mantenimiento> obtenerPorUsuario(Usuario usuario) {
+        return mantenimientoRepository.findByUsuario(usuario);
+    }
+
+    @Override
+    public List<Mantenimiento> obtenerPorTicket(Ticket ticket) {
+        return mantenimientoRepository.findByTicket(ticket);
+    }
+
+    @Override
+    public Mantenimiento actualizar(Mantenimiento mantenimiento) {
         return mantenimientoRepository.save(mantenimiento);
     }
 
