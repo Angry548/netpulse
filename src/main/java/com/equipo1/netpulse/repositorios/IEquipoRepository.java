7a4e6f575c216cb2fac6c198ec87984e11f76be7
Angry548 <angelmate827@gmail.com>
2026-08-27 21:00:41 -0600

    feat(repository) Jose Angel Mate Carias : Modificacion del repositorio deI IEquipoRepository

--- src/main/java/com/equipo1/netpulse/repositorios/IEquipoRepository.java
+++ src/main/java/com/equipo1/netpulse/repositorios/IEquipoRepository.java
@@ -1,11 +1,21 @@
 package com.equipo1.netpulse.repositorios;
 
 import com.equipo1.netpulse.modelos.Equipo;
+import com.equipo1.netpulse.modelos.EstadoEquipo;
+import com.equipo1.netpulse.modelos.TipoEquipo;
+import com.equipo1.netpulse.modelos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IEquipoRepository extends JpaRepository<Equipo, Integer> {
 
     Optional<Equipo> findByNumeroSerie(String numeroSerie);
+
+    List<Equipo> findByTipoEquipo(TipoEquipo tipoEquipo);
+
+    List<Equipo> findByEstadoEquipo(EstadoEquipo estadoEquipo);
+
+    List<Equipo> findByResponsable(Usuario usuario);
 }
