683d578b77f09620915ead5b58a2dae94a855c70
Angry548 <angelmate827@gmail.com>
2026-08-27 21:09:18 -0600

    feat(service) Jose Angel Mate Carias : Modificacion del servicio de EquipoService

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/EquipoService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/EquipoService.java
@@ -1,9 +1,14 @@
 package com.equipo1.netpulse.servicios.implementaciones;
 
 import com.equipo1.netpulse.modelos.Equipo;
+import com.equipo1.netpulse.modelos.EstadoEquipo;
+import com.equipo1.netpulse.modelos.TipoEquipo;
+import com.equipo1.netpulse.modelos.Usuario;
 import com.equipo1.netpulse.repositorios.IEquipoRepository;
 import com.equipo1.netpulse.servicios.interfaces.IEquipoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,12 +31,32 @@ public class EquipoService implements IEquipoService {
 
     @Override
     public Equipo buscarPorNumeroSerie(String numeroSerie) {
-        return equipoRepository.findByNumeroSerie(numeroSerie).orElse(null);
+        return equipoRepository.findByNumeroSerie(numeroSerie).get();
     }
 
     @Override
     public List<Equipo> obtenerTodos() {
         return equipoRepository.findAll();
+    }
+
+    @Override
+    public Page<Equipo> buscarTodosPaginados(Pageable pageable) {
+        return equipoRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Equipo> obtenerPorTipo(TipoEquipo tipoEquipo) {
+        return equipoRepository.findByTipo(tipoEquipo);
+    }
+
+    @Override
+    public List<Equipo> obtenerPorEstado(EstadoEquipo estadoEquipo) {
+        return equipoRepository.findByEstado(estadoEquipo);
+    }
+
+    @Override
+    public List<Equipo> obtenerPorResponsable(Usuario usuario) {
+        return equipoRepository.findByResponsable(usuario);
     }
 
     @Override
