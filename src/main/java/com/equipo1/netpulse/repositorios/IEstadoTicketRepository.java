4458ebf39d0b3875145984522342b1ff53befa33
Angry548 <angelmate827@gmail.com>
2026-09-02 06:15:59 -0600

    feat(service, repository) Jose Angel Mate Carias : Modificacion del servicio y repositrio Estado Ticket

--- src/main/java/com/equipo1/netpulse/repositorios/IEstadoTicketRepository.java
+++ src/main/java/com/equipo1/netpulse/repositorios/IEstadoTicketRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface IEstadoTicketRepository extends JpaRepository<EstadoTicket, Integer> {
+public interface IEstadoTicketRepository
+        extends JpaRepository<EstadoTicket, Integer> {
 
     Optional<EstadoTicket> findByNombre(String nombre);
 
--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/EstadoTicketService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/EstadoTicketService.java
@@ -3,51 +3,70 @@ package com.equipo1.netpulse.servicios.implementaciones;
 import com.equipo1.netpulse.modelos.EstadoTicket;
 import com.equipo1.netpulse.repositorios.IEstadoTicketRepository;
 import com.equipo1.netpulse.servicios.interfaces.IEstadoTicketService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EstadoTicketService implements IEstadoTicketService {
 
-    @Autowired
-    private IEstadoTicketRepository estadoTicketRepository;
+    private final IEstadoTicketRepository estadoTicketRepository;
+
+    public EstadoTicketService(
+            IEstadoTicketRepository estadoTicketRepository) {
+
+        this.estadoTicketRepository = estadoTicketRepository;
+    }
 
     @Override
     public EstadoTicket crear(EstadoTicket estado) {
+
         return estadoTicketRepository.save(estado);
     }
 
     @Override
     public EstadoTicket buscarPorId(Integer id) {
-        return estadoTicketRepository.findById(id).get();
+
+        Optional<EstadoTicket> estado =
+                estadoTicketRepository.findById(id);
+
+        return estado.orElse(null);
     }
 
     @Override
     public EstadoTicket buscarPorNombre(String nombre) {
-        return estadoTicketRepository.findByNombre(nombre).orElse(null);
+
+        Optional<EstadoTicket> estado =
+                estadoTicketRepository.findByNombre(nombre);
+
+        return estado.orElse(null);
     }
 
     @Override
     public List<EstadoTicket> obtenerTodos() {
+
         return estadoTicketRepository.findAll();
     }
 
     @Override
-    public Page<EstadoTicket> buscarTodosPaginados(Pageable pageable) {
+    public Page<EstadoTicket> buscarTodosPaginados(
+            Pageable pageable) {
+
         return estadoTicketRepository.findAll(pageable);
     }
 
     @Override
     public EstadoTicket actualizar(EstadoTicket estado) {
+
         return estadoTicketRepository.save(estado);
     }
 
     @Override
     public void eliminarPorId(Integer id) {
+
         estadoTicketRepository.deleteById(id);
     }
 }
