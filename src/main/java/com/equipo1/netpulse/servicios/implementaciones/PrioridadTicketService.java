b8b22f02ae0fd76889ae6d82a36c40492b2d43dc
Angry548 <angelmate827@gmail.com>
2026-09-02 06:28:26 -0600

    feat(service, repository) Jose Angel Mate Carias : Modificacion del servicio y repositrio Prioridad Ticket

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/PrioridadTicketService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/PrioridadTicketService.java
@@ -3,7 +3,6 @@ package com.equipo1.netpulse.servicios.implementaciones;
 import com.equipo1.netpulse.modelos.PrioridadTicket;
 import com.equipo1.netpulse.repositorios.IPrioridadTicketRepository;
 import com.equipo1.netpulse.servicios.interfaces.IPrioridadTicketService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,41 +12,56 @@ import java.util.List;
 @Service
 public class PrioridadTicketService implements IPrioridadTicketService {
 
-    @Autowired
-    private IPrioridadTicketRepository prioridadTicketRepository;
+    private final IPrioridadTicketRepository prioridadTicketRepository;
+
+    public PrioridadTicketService(
+            IPrioridadTicketRepository prioridadTicketRepository) {
+
+        this.prioridadTicketRepository = prioridadTicketRepository;
+    }
 
     @Override
     public PrioridadTicket crear(PrioridadTicket prioridad) {
+
         return prioridadTicketRepository.save(prioridad);
     }
 
     @Override
     public PrioridadTicket buscarPorId(Integer id) {
-        return prioridadTicketRepository.findById(id).get();
+
+        return prioridadTicketRepository.findById(id).orElse(null);
     }
 
     @Override
     public PrioridadTicket buscarPorNombre(String nombre) {
-        return prioridadTicketRepository.findByNombre(nombre).orElse(null);
+
+        return prioridadTicketRepository
+                .findByNombre(nombre)
+                .orElse(null);
     }
 
     @Override
     public List<PrioridadTicket> obtenerTodos() {
+
         return prioridadTicketRepository.findAll();
     }
 
     @Override
-    public Page<PrioridadTicket> buscarTodosPaginados(Pageable pageable) {
+    public Page<PrioridadTicket> buscarTodosPaginados(
+            Pageable pageable) {
+
         return prioridadTicketRepository.findAll(pageable);
     }
 
     @Override
     public PrioridadTicket actualizar(PrioridadTicket prioridad) {
+
         return prioridadTicketRepository.save(prioridad);
     }
 
     @Override
     public void eliminarPorId(Integer id) {
+
         prioridadTicketRepository.deleteById(id);
     }
 }
