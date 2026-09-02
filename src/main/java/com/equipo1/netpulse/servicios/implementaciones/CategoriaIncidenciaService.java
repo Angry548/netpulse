064ce96fe8dd403d29a860bb20de6829475c62d8
Angry548 <angelmate827@gmail.com>
2026-09-02 06:40:50 -0600

    feat(service, repository) Jose Angel Mate Carias : Modificacion del servicio y repositrio Categoria Incidencia

--- src/main/java/com/equipo1/netpulse/servicios/implementaciones/CategoriaIncidenciaService.java
+++ src/main/java/com/equipo1/netpulse/servicios/implementaciones/CategoriaIncidenciaService.java
@@ -3,7 +3,6 @@ package com.equipo1.netpulse.servicios.implementaciones;
 import com.equipo1.netpulse.modelos.CategoriaIncidencia;
 import com.equipo1.netpulse.repositorios.ICategoriaIncidenciaRepository;
 import com.equipo1.netpulse.servicios.interfaces.ICategoriaIncidenciaService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,53 +12,82 @@ import java.util.List;
 @Service
 public class CategoriaIncidenciaService implements ICategoriaIncidenciaService {
 
-    @Autowired
-    private ICategoriaIncidenciaRepository categoriaIncidenciaRepository;
+    private final ICategoriaIncidenciaRepository categoriaIncidenciaRepository;
+
+    public CategoriaIncidenciaService(
+            ICategoriaIncidenciaRepository categoriaIncidenciaRepository) {
+
+        this.categoriaIncidenciaRepository =
+                categoriaIncidenciaRepository;
+    }
 
     @Override
-    public CategoriaIncidencia crear(CategoriaIncidencia categoria) {
+    public CategoriaIncidencia crear(
+            CategoriaIncidencia categoria) {
+
         return categoriaIncidenciaRepository.save(categoria);
     }
 
     @Override
-    public CategoriaIncidencia buscarPorId(Integer id) {
-        return categoriaIncidenciaRepository.findById(id).get();
+    public CategoriaIncidencia buscarPorId(
+            Integer id) {
+
+        return categoriaIncidenciaRepository
+                .findById(id)
+                .orElse(null);
     }
 
     @Override
-    public CategoriaIncidencia buscarPorNombre(String nombre) {
-        return categoriaIncidenciaRepository.findByNombre(nombre).orElse(null);
+    public CategoriaIncidencia buscarPorNombre(
+            String nombre) {
+
+        return categoriaIncidenciaRepository
+                .findByNombre(nombre)
+                .orElse(null);
     }
 
     @Override
     public List<CategoriaIncidencia> obtenerTodos() {
+
         return categoriaIncidenciaRepository.findAll();
     }
 
     @Override
-    public Page<CategoriaIncidencia> buscarTodosPaginados(Pageable pageable) {
+    public Page<CategoriaIncidencia> buscarTodosPaginados(
+            Pageable pageable) {
+
         return categoriaIncidenciaRepository.findAll(pageable);
     }
 
     @Override
-    public CategoriaIncidencia actualizar(CategoriaIncidencia categoria) {
+    public CategoriaIncidencia actualizar(
+            CategoriaIncidencia categoria) {
+
         return categoriaIncidenciaRepository.save(categoria);
     }
 
     @Override
-    public CategoriaIncidencia activar(CategoriaIncidencia categoria) {
+    public CategoriaIncidencia activar(
+            CategoriaIncidencia categoria) {
+
         categoria.setActivo(true);
+
         return categoriaIncidenciaRepository.save(categoria);
     }
 
     @Override
-    public CategoriaIncidencia desactivar(CategoriaIncidencia categoria) {
+    public CategoriaIncidencia desactivar(
+            CategoriaIncidencia categoria) {
+
         categoria.setActivo(false);
+
         return categoriaIncidenciaRepository.save(categoria);
     }
 
     @Override
-    public void eliminarPorId(Integer id) {
+    public void eliminarPorId(
+            Integer id) {
+
         categoriaIncidenciaRepository.deleteById(id);
     }
 }
